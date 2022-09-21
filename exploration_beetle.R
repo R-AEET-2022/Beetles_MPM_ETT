807177f81b96c9074f4c6d5a7f26d1a3240103bb
Estefania Tobajas <estefaniatob@usal.es>
2022-09-21 17:38:23 +0200

    riqueza frente a tiempo (lm)

--- exploration_beetle.R
+++ exploration_beetle.R
@@ -6,6 +6,8 @@ library(summarytools)
 library(knitr)
 library(performance)
 library(effects)
+library(ggplot2)
+library(RColorBrewer)
 
 
 ## Data 
@@ -22,13 +24,25 @@ levels(factor(beetle$GENUS_SPECIES))
 
 bet_sum<-beetle %>%
   group_by(PLOT, YEAR) %>%
-  summarise(n_species = n(),
-            ABUNDANCE = sum(ABUNDANCE))
-
-bet_sum=bet_sum  %>%
+  summarise(n_species = n(),ABUNDANCE = sum(ABUNDANCE))%>%
   rename(plot=PLOT, year=YEAR, abundance= ABUNDANCE)
+
+
 glimpse(bet_sum)
   
 #write.csv(bet_sum, "C:/Users/estef/git/Beetles_MPM_ETT/data_clean/beetle_richness.csv")
+
+
+# ¿cambia la riqueza de especies a lo largo del tiempo?
+ggplot(bet_sum, aes(x = year,y = n_species)) +
+  geom_point(color="blue")
+
+mod_richness = lm(n_species ~ year, data = bet_sum)
+summary(mod_richness)
+
+modelbased::estimate_contrasts(mod_richness)
+plot(allEffects(mod_richness))
+
+performance::check_model(mod_richness)
 
 
